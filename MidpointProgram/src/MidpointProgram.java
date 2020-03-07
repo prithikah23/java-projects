97072255aecabb7f18aaddf4c3214d7f84542322
prithikah23 <60564006+prithikah23@users.noreply.github.com>
2020-03-07 18:43:05 -0500

    Update MidpointProgram.java

--- MidpointProgram/src/MidpointProgram.java
+++ MidpointProgram/src/MidpointProgram.java
@@ -1,12 +1,4 @@
-/*
- * Name: Prithika Hariharan
- * WatID: 20813370
- * Date: 3rd March, 2020
- * Assignment 6, Q3
- * Description: Checks whether one of the values passed is the midpoint of the other two points
- * Input: The method takes input from the user in the form of parameters
- * Output: It returns true or false based on whether the condition is satisfied
- * */
+
 public class MidpointProgram {
 
 	public static void main(String[] args) {
